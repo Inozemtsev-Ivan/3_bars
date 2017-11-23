e8db87ab64ff7b0f2ff48317c245c2a2542126a0
inozemtsev-ivan <inozemtsev.il@gmail.com>
2017-11-23 19:54:44 +0300

    Started form this release: # JSON with bars' data is downloaded any time script is running # user can specify his current coordinates

--- bars.py
+++ bars.py
@@ -1,19 +1,36 @@
-import os
 import sys
-import json
 import math
+import requests
+import http_status
 
-FILE_PATH = 'bars.json'
-LONGITUDE = 37.683185
-LATITUDE = 55.740071
+DATA_MOS_RU_URL = 'https://apidata.mos.ru/v1/features/1796?api_key='
+DATA_MOS_RU_API_KEY = 'd972f4bad54daf8410866cf9472f9efc'
 
 
-def load_data(filepath):
-    if os.path.exists(filepath):
-        with open(file=filepath, mode='r') as datafile:
-            return json.load(datafile)
+def input_coordinates():
+    print('Укажите широту и долготу через пробел, например, "37.6269 55.7535"')
+    while True:
+        elements = input('>').split()
+        try:
+            longitude, latitude = map(float, elements)
+            return longitude, latitude
+        except ValueError:
+            print('Неправильно указаны координаты! Повторите ввод.')
+            continue
+
+
+def load_data(api_address, api_key):
+    url = ''.join([api_address, api_key])
+    r = requests.get(url)
+    if r.status_code == 200:
+        return r.json()
     else:
-        raise FileNotFoundError
+        http_error = http_status.Status(r.status_code)
+        exception_description = 'Error {code}: {desc}'.format(
+            code=http_error.code,
+            desc=http_error.description,
+        )
+        raise IOError(exception_description)
 
 
 def get_biggest_bar(data):
@@ -41,9 +58,10 @@ def print_bar_info(kind_of_bar, bar_representation):
 
 if __name__ == '__main__':
     try:
-        all_bars = load_data(FILE_PATH)
-        print_bar_info('большой', get_biggest_bar(all_bars))
-        print_bar_info('маленький', get_smallest_bar(all_bars))
-        print_bar_info('ближайший', get_closest_bar(all_bars, LONGITUDE, LATITUDE))
-    except FileNotFoundError:
-        sys.exit('File with data is missed!')
+        all_bars = load_data(DATA_MOS_RU_URL, DATA_MOS_RU_API_KEY)
+    except IOError as ex:
+        sys.exit(str(ex))
+    longitude, latitude = input_coordinates()
+    print_bar_info('большой', get_biggest_bar(all_bars))
+    print_bar_info('маленький', get_smallest_bar(all_bars))
+    print_bar_info('ближайший', get_closest_bar(all_bars, longitude, latitude))
